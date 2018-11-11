4f53eb731aa26999d960ba84cc99f5ded184553b
jdmac020 <john.macgregor@gmail.com>
2018-11-11 14:47:49 -0500

    LogScan tests passing

---
+++ ZenCashier/ZenCashier/Domain/Log/IScanLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenCashier.Domain.Log
+{
+    public interface IScanLog
+    {
+        double SubTotal { get; }
+
+        void LogScan(string skuId, double salePrice, double qty);
+
+        IEnumerable<ScannedItemModel> GetScansForSku(string sku);
+    }
+}
